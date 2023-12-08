db581b0f9a6e801dee47f4a33f7228eea8bacdb8
B00147616 <B00147616@mytudublin.ie>
2023-12-08 14:42:56 +0000

    made registeredUsers table which outputs value in SQL

--- login.php
+++ login.php
@@ -4,8 +4,25 @@ $usr = $_POST['user'];
 $pwrd = $_POST['pass'];
 
 
-require('dbcon.php')
+require('dbcon.php');
 
-$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_database);
+$conn = mysqli_connect($mysql_servername, $mysql_username, $mysql_password, $mysql_dbname);
+
+$sql = "select pwrd from registeredUsers where userName = '$usr'";
     
+$result = mysqli_query($conn, $sql);
+
+while($row = $result->fetch_assoc()) 
+{
+    $dBpwrd = $row['pwrd'];
+}
+
+if (dBpwrd == $prwd)
+{
+    echo "the password matches";
+}
+else
+{
+    echo "the password does not match";
+}
 ?>
